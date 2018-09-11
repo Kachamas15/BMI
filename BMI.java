b99062780da8fb690c56f42a5cc33adb41617f1d
Unknown <60050132@kmitl.ac.th>
2018-09-11 13:56:09 +0700

    BMINew

--- BMI.java
+++ BMI.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 class BMI
 {
 	public static void main(String[] args) 
-	{ Scanner sc=new Scanner(System.in);
+	  { Scanner sc=new Scanner(System.in);
 		System.out.print("Enter  Weight: ");
 		    double w=sc.nextDouble();
 		System.out.print("Enter Height: ");
 		    double h=sc.nextDouble();
 			double H=h/100;
-			    H=H*H;
+			       H=H*H;
 
-		double bmi=w/H;
+		    double bmi=w/H;
         System.out.print("BMI = "+bmi);
 
-	}
+	  }
 }
